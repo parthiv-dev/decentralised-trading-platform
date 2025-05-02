b47ec434c0b6faedf861ce3fb887d14e80de387c
parthiv-dev <parthivkangsabanik@gmail.com>
2025-05-02 20:12:04 +0200

    Commit before deleting python related files

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -1,6 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 
+// Importing the ERC721 contract from OpenZeppelin
+// import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
